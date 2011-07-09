21b69075af85ed0db124aa9848a150344ace85b8
DarkSkeleton2010 <DarkSkeleton2010@173c8494-34e9-b742-4727-882504d3d79f>
2011-07-09 09:32:29 +0000

    Обновление от 09.07.2011

---
+++ trunk/LoginServer/src/Services/DatabaseSetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TeamSystem.Data.UnitTesting;
+
+namespace PointBlankServer.src.Services
+{
+    [TestClass()]
+    public class DatabaseSetup
+    {
+
+        [AssemblyInitialize()]
+        public static void IntializeAssembly(TestContext ctx)
+        {
+            //   Выполнить настройку базы данных тестирования, используя
+            // файл конфигурации
+            DatabaseTestClass.TestService.DeployDatabaseProject();
+            DatabaseTestClass.TestService.GenerateData();
+        }
+
+    }
+}
