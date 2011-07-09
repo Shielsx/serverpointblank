e28cf6308a853243f1e334387a66015e10d8dbb6
DarkSkeleton2010 <DarkSkeleton2010@173c8494-34e9-b742-4727-882504d3d79f>
2011-07-09 09:37:59 +0000

    Программа для авторизации в pb

---
+++ trunk/PBAuth/Refresh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PBAuth
+{
+    public partial class Refresh : Form
+    {
+        public Refresh()
+        {
+            InitializeComponent();
+        }
+    }
+}
