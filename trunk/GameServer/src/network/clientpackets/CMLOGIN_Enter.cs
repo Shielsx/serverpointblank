047fa1141c78aac698740a691e6c52b07746aaea
DarkSkeleton2010 <DarkSkeleton2010@173c8494-34e9-b742-4727-882504d3d79f>
2011-07-09 09:32:08 +0000

    Обновление от 09.07.2011

---
+++ trunk/GameServer/src/network/clientpackets/CMLOGIN_Enter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointBlankServer.src.network.clientpackets
+{
+    class CMLOGIN_Enter
+    {
+        //public static byte[] Login_Puzzle2 = new byte[8] { 0x04, 0x00, 0x04, 0x0a, 0x07, 0x01, 0x00, 0x80 };
+        public static byte[] CMLogin_Enter = new byte[4] { 0x00, 0x00, 0x11, 0x0a };
+
+        
+
+        public static byte[] packetbyte()
+        {
+            return CMLogin_Enter;
+        }
+
+        public static int packetlength()
+        {
+            return CMLogin_Enter.Length;
+        }
+
+    }
+}
