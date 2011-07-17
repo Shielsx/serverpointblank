36d6b986035074bb4f88ae0d43acd0f7f893f871
DarkSkeleton2010@gmail.com <DarkSkeleton2010@gmail.com@173c8494-34e9-b742-4727-882504d3d79f>
2011-07-17 07:31:54 +0000

    Обновление от 09.07.2011

---
+++ trunk/LoginServer/src/Services/consolemessages.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using PointBlankServer.src.Services;
+
+
+//using СlientandserverTest.src.services.encrypt.encryptservice;
+
+namespace PointBlankServer.src.services.consolemessages
+{
+    class consolemessages
+    {
+        public static string message = null;
+
+        public static bool consolemessagesstart()
+        {
+            Console.WriteLine("Console commands:");
+            Console.WriteLine("shutdown - this command shutdown LoginServer");
+            return true;
+        }
+
+        public static void run()
+        {
+
+            do
+            {
+                message = Console.ReadLine();
+                if (message != null)
+                {
+                    switch (message)
+                    {
+                        case "shutdown":
+                            ConsoleUtil.consoleinfo("SERVER IS SHUTTING DOWN!");
+                            ShutDown.ShutDownAll();
+                            break;
+                    }
+                }
+            }
+            while (message != null);
+        }
+    }
+}
---
+++ trunk/LoginServer/src/Services/test2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointBlankServer.src.Services
+{
+    public class Functions
+    {
+        private static readonly char[] HexChars = "0123456789ABCDEF".ToCharArray();
+
+        public static string BytesToHex(byte[] data)
+        {
+            StringBuilder builder = new StringBuilder(data.Length * 2);
+            foreach (byte b in data)
+            {
+                builder.Append(HexChars[b >> 4]);
+                builder.Append(HexChars[b & 0xf]);
+            }
+            return builder.ToString();
+        }
+
+        public static byte[] HexToBytes(String hexString)
+        {
+            byte[] result = new byte[hexString.Length / 2];
+            for (int j = 1; j < hexString.Length; )
+            {
+                result[j / 2] = Convert.ToByte(Convert.ToInt32("0x0" + hexString.Substring(j - 1, 2), 16));
+                j += 2;
+            }
+            return result;
+        }
+
+        public static string HexToString(String hexString)
+        {
+            String result = "";
+            for (int j = 1; j < hexString.Length; )
+            {
+                result += Convert.ToByte(Convert.ToInt32("0x0" + hexString.Substring(j - 1, 2), 16));
+                j += 2;
+            }
+            return result;
+        }
+
+        public static double IPAddressToNumber(string IPaddress)
+        {
+            if (IPaddress == "")
+                return 0;
+
+            int i;
+            string[] arrDec;
+            double num = 0;
+
+            arrDec = IPaddress.Split('.');
+            for (i = arrDec.Length - 1; i >= 0; i = i - 1)
+            {
+                num += ((int.Parse(arrDec[i]) % 256) * Math.Pow(256, (3 - i)));
+            }
+            return num;
+        }
+    }
+}
